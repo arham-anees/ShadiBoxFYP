1fa767332bc2b733c2a78d9dcd709a80edf94485
arham-anees <arhamanees10@gmail.com>
2020-02-18 17:44:46 +0500

    City added to db
    mapper for city and migration done

--- Mapper/cCityMapper.cs
+++ Mapper/cCityMapper.cs
@@ -4,10 +4,23 @@ using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLogicLayer;
 using BusinessObjectLayer;
 
 namespace Mapper {
-	public class cCityMapper :EntityTypeConfiguration<iCity> {
+	public class cCityMapper :EntityTypeConfiguration<cCity> {
+		public cCityMapper()
+		{
+			this.HasKey(x => x.Id);
+			this.HasIndex(x => x.Name)
+				.IsUnique(true);
 
+			this.Property(x => x.Name)
+				.IsRequired()
+				.HasMaxLength(20);
+			this.Property(x => x.Description)
+				.IsOptional()
+				.HasMaxLength(100);
+		}
 	}
 }
---
+++ PersistenceLayer/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogicLayer;
+using BusinessObjectLayer;
+using Mapper;
+
+namespace PersistenceLayer {
+	public class AppDbContext :DbContext{
+
+		#region DbSets
+
+		public DbSet<cCity> Cities { get; set; }
+
+
+		#endregion
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Configurations
+				.Add(new cCityMapper());
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
